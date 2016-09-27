490a472a5c245fe8aeef1d203e81127a781e8ed8
Eric Rasche <rasche.eric@gmail.com>
2016-09-27 19:35:41 +0000

    Only list accessible orgs

--- list_organisms.py
+++ list_organisms.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 import json
 import argparse
-from webapollo import WAAuth, WebApolloInstance
+from webapollo import WAAuth, WebApolloInstance, AssertUser, accessible_organisms
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample script to add an attribute to a feature via web services')
     WAAuth(parser)
+    parser.add_argument('email', help='User Email')
     args = parser.parse_args()
 
     wa = WebApolloInstance(args.apollo, args.username, args.password)
-    print json.dumps(wa.organisms.findAllOrganisms(), indent=2)
+
+    gx_user = AssertUser(wa.users.loadUsers(email=args.email))
+    all_orgs = wa.organisms.findAllOrganisms()
+
+    orgs = accessible_organisms(gx_user, all_orgs)
+
+    print json.dumps(orgs, indent=2)
