d66c15770354fd0b136e94a1a959fc7f4dc1d4df
Yating Liu <yatinghust@gmail.com>
2018-03-06 17:48:47 -0600

    output csv file with header and operation information

--- tools/apollo/apollo_user_manager.py
+++ tools/apollo/apollo_user_manager.py
@@ -7,6 +7,7 @@ import time
 import json
 import logging
 import codecs
+import csv
 from builtins import range, str
 
 from webapollo import WAAuth, WebApolloInstance
@@ -18,7 +19,7 @@ def pwgen(length):
     chars = list('qwrtpsdfghjklzxcvbnm')
     return ''.join(random.choice(chars) for _ in range(length))
 
-def createApolloUser(user):
+def createApolloUser(user, out):
     password = pwgen(12)
     time.sleep(1)
     users = wa.users.loadUsers()
@@ -29,18 +30,19 @@ def createApolloUser(user):
         # Update name, regen password if the user ran it again
         userObj = apollo_user[0]
         returnData = wa.users.updateUser(userObj, user['useremail'], user['firstname'], user['lastname'], password)
-        print('Updated User\nUsername: %s\nPassword: %s' % (user['useremail'], password))
+        out.writerow({'Operation':'Update User', 'First Name': user['firstname'], 'Last Name': user['lastname'],
+                       'Email': user['useremail'], 'New Password': password})
     else:
         returnData = wa.users.createUser(user['useremail'], user['firstname'], user['lastname'], password, role='user')
-        print('Created User\nUsername: %s\nPassword: %s' % (user['useremail'], password))
+        out.writerow({'Operation':'Create User', 'First Name': user['firstname'], 'Last Name': user['lastname'],
+                      'Email': user['useremail'], 'Password': password})
+    print("Return data: " + str(returnData) + "\n")
 
-    print("Return data: " + str(returnData))
 
-
-def createApolloUsers(users_list):
+def createApolloUsers(users_list, out):
     for user in users_list:
         if user['batch'] == "false":
-            createApolloUser(user)
+            createApolloUser(user, out)
         elif user['batch'] == "true":
             users = parseUserInfoFile(user['format'], user['false_path'])
             for u in users:
@@ -52,30 +54,34 @@ def createApolloUsers(users_list):
                     logger.error("Cannot find lastname in the text file, make sure you use the correct header, see README file for examples.")
                 if not 'password' in u:
                     logger.error("Cannot find password in the text file, make sure you use the correct header, see README file for examples.")
-                createApolloUser(u)
+                createApolloUser(u, out)
 
 
-def deleteApolloUser(user):
+def deleteApolloUser(user, out):
     u = wa.users.loadUsers(email=user['useremail'])
     if len(u) == 1:
         returnData = wa.users.delete_user(u)
     else:
         logger.error("The user %s doesn't exist", user['useremail'])
-    print("Return data: " + str(returnData))
+    out.writerow({'Operation':'Delete User', 'First Name': u.firstName, 'Last Name': u.lastName,
+                  'Email': user['useremail']})
+    #print("Delete user\nUsername: %s\n", user['useremail'])
+    print("Return data: " + str(returnData) + "\n")
 
 
-def deleteApolloUsers(users_list):
+def deleteApolloUsers(users_list, out):
+    out.writerow(['Operation', 'Username'])
     for user in users_list:
         if user['batch'] == "false":
-            deleteApolloUser(user)
+            deleteApolloUser(user, out)
         elif user['batch'] == "true":
             users = parseUserInfoFile(user['format'], user['false_path'])
             for u in users:
                 if not 'useremail' in u:
                     logger.error("Cannot find useremail in the text file, make sure you use the correct header, see README file for examples.")
-                deleteApolloUser(u)
+                deleteApolloUser(u, out)
 
-def addApolloUserToGroup(user):
+def addApolloUserToGroup(user, out):
     u = wa.users.loadUsers(email=user['useremail'])
     group = wa.groups.loadGroupByName(users_list['group'])
     if not u:
@@ -83,12 +89,15 @@ def addApolloUserToGroup(user):
     if not group:
         logger.error("the group %s doesn't exist", user['group'])
     returnData = wa.users.addUserToGroup(group, u)
-    print("Return data: " + str(returnData))
+    #print("Add user to the group\nUsername: %s, Group: %s\n", user['useremail'], user['group'])
+    out.writerow({'Operation':'Add User to Group', 'First Name': u.firstName, 'Last Name': u.lastName,
+                  'Email': user['useremail'], 'Add to Group': group.name})
+    print("Return data: " + str(returnData) + "\n")
 
-def addApolloUsersToGroups(users_list):
+def addApolloUsersToGroups(users_list, out):
     for user in users_list:
         if user['batch'] == "false":
-            addApolloUserToGroup(user)
+            addApolloUserToGroup(user, out)
         elif user['batch'] == "true":
             users = parseUserInfoFile(user['format'], user['false_path'])
             for u in users:
@@ -96,10 +105,10 @@ def addApolloUsersToGroups(users_list):
                     logger.error("Cannot find useremail in the text file, make sure you use the correct header, see README file for examples.")
                 if not 'group' in u:
                     logger.error("Cannot find group in the text file, make sure you use the correct header, see README file for examples.")
-                addApolloUserToGroup(u)
+                addApolloUserToGroup(u, out)
 
 
-def removeApolloUserFromGroup(user):
+def removeApolloUserFromGroup(user, out):
     u = wa.users.loadUsers(email=user['useremail'])
     group = wa.groups.loadGroupByName(users_list['group'])
     if not u:
@@ -107,12 +116,15 @@ def removeApolloUserFromGroup(user):
     if not group:
         logger.error("the group %s doesn't exist", user['group'])
     returnData = wa.users.removeUserFromGroup(group, user)
-    print("Return data: " + str(returnData))
+    out.writerow({'Operation':'Remove User from Group', 'First Name': u.firstName, 'Last Name': u.lastName,
+                  'Email': user['useremail'], 'Remove from Group': group.name})
+    print("Remove user from group\nUsername: %s, Group: %s\n", user['useremail'], user['group'])
+    #print("Return data: " + str(returnData) + "\n")
 
-def removeApolloUsersFromGroups(users_list):
+def removeApolloUsersFromGroups(users_list, out):
     for user in users_list:
         if user['batch'] == "false":
-            removeApolloUserFromGroup(user)
+            removeApolloUserFromGroup(user, out)
         elif user['batch'] == "true":
             users = parseUserInfoFile(user['format'], user['false_path'])
             for u in users:
@@ -120,7 +132,7 @@ def removeApolloUsersFromGroups(users_list):
                     logger.error("Cannot find useremail in the text file, make sure you use the correct header, see README file for examples.")
                 if not 'group' in u:
                     logger.error("Cannot find group in the text file, make sure you use the correct header, see README file for examples.")
-                removeApolloUserFromGroup(u)
+                removeApolloUserFromGroup(u, out)
 
 
 def parseUserInfoFile(self, file_format, filename):
@@ -150,7 +162,7 @@ def loadJson(jsonFile):
         data_file = codecs.open(jsonFile, 'r', 'utf-8')
         return json.load(data_file)
     except IOError:
-        print("Cannot find JSON file\n")
+        logger.error("Cannot find JSON file\n")
         exit(1)
 
 
@@ -159,20 +171,26 @@ if __name__ == '__main__':
     WAAuth(parser)
 
     parser.add_argument('-j', '--data_json', help='JSON file containing the metadata of the inputs')
+    parser.add_argument('-o', '--output', help='HTML output')
+
     args = parser.parse_args()
     jsonData = loadJson(args.data_json)
+    outputFile = open(args.output, 'a')
+    fieldnames = ['Operation', 'First Name', 'Last Name', 'Email', 'New Password', 'Add to Group', 'Remove from Group']
+    csvWriter = csv.DictWriter(outputFile, fieldnames=fieldnames)
+    csvWriter.writeheader()
     operations_dictionary = jsonData.get("operations")
     wa = WebApolloInstance(args.apollo, args.username, args.password)
 
     for operation, users_list in operations_dictionary.items():
         if operation == "create":
-            createApolloUsers(users_list)
+            createApolloUsers(users_list, csvWriter)
         elif operation == "delete":
-            deleteApolloUsers(users_list)
+            deleteApolloUsers(users_list, csvWriter)
         elif operation == "add":
-            addApolloUsersToGroups(users_list)
+            addApolloUsersToGroups(users_list, csvWriter)
         elif operation == "remove":
-            removeApolloUsersFromGroups(users_list)
+            removeApolloUsersFromGroups(users_list, csvWriter)
 
-
+    outputFile.close()
 
