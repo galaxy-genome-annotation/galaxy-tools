a8809083f704d3acc857cc72200a47b668acc4a7
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-12-14 14:46:43 +0100

    address comments

--- tools/genenotebook/find_free_port.sh
+++ tools/genenotebook/find_free_port.sh
@@ -6,17 +6,38 @@ set -e
 # Should work in biocontainers derived from conda
 # Taken from https://unix.stackexchange.com/a/358101
 
-netstat -aln | awk '
-  $6 == "LISTEN" {
-    if ($4 ~ "[.:][0-9]+$") {
-      split($4, a, /[:.]/);
-      port = a[length(a)];
-      p[port] = 1
+if command -v ss &> /dev/null
+then
+  ss -alnut | awk '
+    $2 == "LISTEN" {
+      if ($5 ~ "[.:][0-9]+$") {
+        split($5, a, /[:.]/);
+        port = a[length(a)];
+        p[port] = 1
+      }
     }
-  }
-  END {
-    for (i = 7000; i < 65000 && p[i]; i++){};
-    if (i == 65000) {exit 1};
-    print i
-  }
-'
+    END {
+      for (i = 7000; i < 65000 && p[i]; i++){};
+      if (i == 65000) {exit 1};
+      print i
+    }
+  '
+elif command -v netstat &> /dev/null
+then
+  netstat -aln | awk '
+    $6 == "LISTEN" {
+      if ($4 ~ "[.:][0-9]+$") {
+        split($4, a, /[:.]/);
+        port = a[length(a)];
+        p[port] = 1
+      }
+    }
+    END {
+      for (i = 7000; i < 65000 && p[i]; i++){};
+      if (i == 65000) {exit 1};
+      print i
+    }
+  '
+else
+  echo "This tool requires one of 'netstat' of 'ss' commands."
+fi
